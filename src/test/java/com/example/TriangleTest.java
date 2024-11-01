6c017fd4a63806a71233817357806d6ca9955a51
Gabriella-liz <24093471g@connect.polyu.hk>
2024-11-01 06:46:32 +0000

    update answer

--- src/test/java/com/example/TriangleTest.java
+++ src/test/java/com/example/TriangleTest.java
@@ -20,15 +20,39 @@ public class TriangleTest {
     public void testIsoceles() {
         Triangle t1 = new Triangle(3, 3, 5);
         assertEquals("Isosceles", t1.getTriangleType());
+
+        Triangle t2 = new Triangle(3, 5, 3);
+        assertEquals("Isosceles", t2.getTriangleType());
+
+        Triangle t3 = new Triangle(5, 3, 3);
+        assertEquals("Isosceles", t3.getTriangleType());
     }
 
     @Test
     public void testNotTriangle1() {
         //TODO: add your test cases 
+        Triangle t1 = new Triangle(3, 3, 90);
+        assertEquals("Error: Not a triangle", t1.getTriangleType());
+
+        Triangle t2 = new Triangle(3, 90, 3);
+        assertEquals("Error: Not a triangle", t2.getTriangleType());
+
+        Triangle t3 = new Triangle(90, 3, 2);
+        assertEquals("Error: Not a triangle", t3.getTriangleType());
+
+        
     }
 
     @Test
     public void sidesShouldBePositive() {
         //TODO: add your test cases 
+        Triangle t1 = new Triangle(-3, 3, 5);
+        assertEquals("Error: The length should be positive", t1.getTriangleType());
+
+         Triangle t2 = new Triangle(3, -3, 5);
+        assertEquals("Error: The length should be positive", t2.getTriangleType());
+
+         Triangle t3 = new Triangle(3, 3, -5);
+        assertEquals("Error: The length should be positive", t3.getTriangleType());
     }
 }
